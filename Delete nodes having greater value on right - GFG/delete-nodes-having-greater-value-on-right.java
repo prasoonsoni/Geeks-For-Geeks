57b4ab51ee7c0875b95701d8d4085400586e3db8
Prasoon Soni <prasoonsoni.work@gmail.com>
2022-06-29 23:33:15 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,115 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        head = reverse(head);
+        Node temp = head;
+        Node tail = null;
+        int max = Integer.MIN_VALUE;
+        while(temp != null){
+            if(temp.data >= max){
+                max = temp.data;
+                tail = temp;
+                temp = temp.next;
+            } else {
+                tail.next = temp.next;
+                temp = temp.next;
+            }
+        }
+        return reverse(head);
+    }
+    
+    Node reverse(Node head)
+    {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+  
