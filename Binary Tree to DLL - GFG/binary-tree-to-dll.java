07ea6c0f2568464caff96689b7c9d60dbffcbb62
Prasoon Soni <prasoonsoni.work@gmail.com>
2022-07-01 18:07:51 +0530

    Added solution - LeetHub

---
+++ Binary Tree to DLL - GFG/binary-tree-to-dll.java
@@ -0,0 +1,181 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}
+class BT_To_DLL
+{
+    
+    static Node buildTree(String str)throws IOException{
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+
+	void inorder(Node node)
+	{
+		if(node==null)
+			return ;
+		else
+			inorder(node.left);
+			System.out.print(node.data + " ");
+			inorder(node.right);
+	}
+	public static void printList(Node head) 
+    {
+		Node prev = head;
+        while (head != null) 
+        {
+            System.out.print(head.data + " ");
+			prev = head;
+            head = head.right;
+        }
+		
+		System.out.println();
+		while(prev != null)
+		{
+			System.out.print(prev.data+" ");
+			prev = prev.left;
+		}
+    }
+	public static void main(String args[])  throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			    
+			    Node ans = g.bToDLL(root);
+			    printList(ans);
+                t--;
+                System.out.println();
+	        }
+			
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+class Node
+{
+	Node left, right;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+	
+}*/
+
+//This function should return head to the DLL
+
+class Solution
+{
+    void inorder(Node node, ArrayList<Integer> data){
+        if(node==null){
+            return;
+        }
+        inorder(node.left, data);
+        data.add(node.data);
+        inorder(node.right, data);
+    }
+    Node bToDLL(Node root)
+    {
+	    ArrayList<Integer> data = new ArrayList<Integer>();
+	    inorder(root, data);
+	    if(data.size()==0){
+	        return null;
+	    }
+	    Node head = new Node(data.get(0));
+	    head.left = null;
+	    head.right = null;
+	    Node curr = head;
+	    for(int i=1;i<data.size();i++){
+	        Node new_node = new Node(data.get(i));
+	        new_node.right = null;
+	        new_node.left = curr;
+	        
+	        curr.right = new_node;
+	        curr = new_node;
+	    }
+	    
+	    return head;
+    }
+}
